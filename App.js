45b591a0ffd3c2a46796a4b652883c5fe8a0d617
MOON <50370479+jangmoonwon@users.noreply.github.com>
2021-07-08 22:05:43 +0900

    세팅하기

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <P>다크모드 만들어보기</P>
     </div>
   );
 }
 
 export default App;
+
+
+const P = styled.p`
+font-size: 30px;
+`;
